6cc24c940ba5bc463fc532edea51be26f52fef81
jugglingdev <kaylamarie1785@gmail.com>
2023-09-28 22:36:27 -0500

    Completed in-class exercise 2

--- angular-basics-exercises/src/app/article/article.component.ts
+++ angular-basics-exercises/src/app/article/article.component.ts
@@ -7,9 +7,34 @@ import { Component } from "@angular/core";
 })
 
 export class ArticleComponent {
-  title: string = 'What a Wonderful Day';
-  content: string =
-    'I see trees of green, Red roses too, I see them bloom, For me and you.  And I think to myself What a wonderful world.';
+  title: string = 'What a Codefied World';
+  content: string =`
+    (Verse 1)
+    I see codebases so vast and wide,
+    Bugs to squash and functions to confide,
+    And I think to myself, what a codefied world.
+
+    (Verse 2)
+    The pull requests, they come and they go,
+    Through Git and GitHub, and make the code grow,
+    And I think to myself, what a codefied world.
+
+    (Chorus)
+    The colors of syntax so vivid and bright,
+    Bring all my ideas to life in the night,
+    I see friends coding apps, saying 'How do you do?',
+    They're really saying 'I broke it, too'.
+
+    (Verse 3)
+    Libraries, frameworks, tools we embrace,
+    With every line of code, we find our place,
+    And I think to myself, what a codefied world.
+
+    (Outro)
+    Yes, I think to myself, what a codefied world,
+    Oh, yes, I think to myself, what a codefied world.
+    `;
+
   isTechRelated: boolean = false;
 
   getBackgroundColor() {
